96ddf19ce92ba31335402b068363a646a063de40
YC Wong <ycwong360@gmail.com>
2022-06-26 09:41:03 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post, Comment
 from blog.forms import CommentForm
 from django.utils import timezone
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+from django.http import HttpResponse
 
 logger = logging.getLogger(__name__)
+# print("__name__:", __name__)
 
 # Create your views here.
+@cache_page(300)
+@vary_on_cookie
 def index(request):
+  logger.info("Index function is called.")
+  logger.info(f"User {str(request.user)} sending request to retrieve index page.")
+  # return HttpResponse(str(request.user).encode("ascii"))
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  logger.info('Got %d posts', len(posts))
   return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
