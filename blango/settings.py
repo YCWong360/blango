c50c41ba49eda12412c0c72bd044b90b1acf745e
YC Wong <ycwong360@gmail.com>
2022-07-02 06:35:45 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -47,6 +47,7 @@ class Dev(Configuration):
       'django.contrib.sessions',
       'django.contrib.messages',
       'django.contrib.staticfiles',
+      'blango_auth',
       'blog',
       "crispy_forms",
       "crispy_bootstrap5",
@@ -63,6 +64,8 @@ class Dev(Configuration):
       'django.contrib.messages.middleware.MessageMiddleware',
       # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
   ]
+
+  AUTH_USER_MODEL = "blango_auth.User"
 
   INTERNAL_IPS = ['192.168.11.179']
   ROOT_URLCONF = 'blango.urls'
